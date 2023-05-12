89d7ff3750106e23d2ab653bb00f3d90c9cdfbbe
Daniel <foxik123@onet.pl>
2023-05-12 17:40:07 +0200

    git add . git commit -m "funcja record" git fetch git merge origin/master git push

--- src/main/java/pl/edu/pk/student/carrots/cinema/models/Movie.java
+++ src/main/java/pl/edu/pk/student/carrots/cinema/models/Movie.java
@@ -1,3 +1,5 @@
 package pl.edu.pk.student.carrots.cinema.models;
 
 public record Movie(String title, String description, Integer duration, Boolean ageRestricted) {}
+
+
--- src/main/java/pl/edu/pk/student/carrots/cinema/models/Snack.java
+++ src/main/java/pl/edu/pk/student/carrots/cinema/models/Snack.java
@@ -1,3 +1,7 @@
 package pl.edu.pk.student.carrots.cinema.models;
 
-public record Snack(String name, Integer price, Integer quantity) {}
+public record Snack(String name, Integer price, Integer quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+}
