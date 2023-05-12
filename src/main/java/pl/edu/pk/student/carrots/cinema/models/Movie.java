0d4b351130a3bbb662fa5f14d2cbac7dc5f5c8ef
Daniel <foxik123@onet.pl>
2023-05-12 17:55:13 +0200

    added this to function

--- src/main/java/pl/edu/pk/student/carrots/cinema/models/Movie.java
+++ src/main/java/pl/edu/pk/student/carrots/cinema/models/Movie.java
@@ -1,5 +1,10 @@
 package pl.edu.pk.student.carrots.cinema.models;
 
-public record Movie(String title, String description, Integer duration, Boolean ageRestricted) {}
+public record Movie(String title, String description, Integer duration, Boolean ageRestricted) {
+    this.title = title;
+    this.description = description;
+    this.duration = duration;
+    this.ageRestricted = ageRestricted;
+}
 
 
