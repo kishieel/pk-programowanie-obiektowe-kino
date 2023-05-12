730a01c1452b052627ab3e8256a3e934e9c8b57d
jakubdziadkowiec17 <jakub.dziadkowiec17@gmail.com>
2023-05-12 17:23:57 +0200

    commit3

---
+++ src/main/java/pl/edu/pk/student/carrots/cinema/actions/RegisterAction.java
@@ -0,0 +1,30 @@
+package pl.edu.pk.student.carrots.cinema.actions;
+import java.util.Scanner;
+public class RegisterAction implements Action
+{
+    @Override
+    public String getTitle() {
+        return "Zarejstruj się";
+    }
+    @Override
+    public void doAction()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter login: ");
+        String login1 = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password1 = scanner.nextLine();
+        System.out.print("Enter name: ");
+        String name1 = scanner.nextLine();
+        System.out.print("Enter surname: ");
+        String surname1 = scanner.nextLine();
+        System.out.print("Enter email: ");
+        String email1 = scanner.nextLine();
+        System.out.print("Enter phone number: ");
+        int phone1 = Integer.parseInt(scanner.nextLine());
+
+        User user1 = new User(login1, password1, name1, surname1, email1, phone1);
+        System.out.println("Registered successfully!");
+
+    }
+}
