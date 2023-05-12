0d18305559865854f3332014bb81c4f0d3fd2a5d
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2023-05-12 16:37:50 +0200

    improve io

--- src/main/java/pl/edu/pk/student/carrots/cinema/utils/IO.java
+++ src/main/java/pl/edu/pk/student/carrots/cinema/utils/IO.java
@@ -3,11 +3,35 @@ package pl.edu.pk.student.carrots.cinema.utils;
 import pl.edu.pk.student.carrots.cinema.actions.Action;
 
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IO {
     public static void menu(String title, List<Action> actions) {
         System.out.println(title);
+        AtomicInteger index = new AtomicInteger(1);
+        actions.forEach(action -> System.out.println((index.getAndIncrement()) + ". " + action.getTitle()));
 
+        while (true) {
+            System.out.print("Wybierz opcje: ");
+            int opt = IO.input(Integer.class);
+            if (opt - 1 >= 0 && opt - 1 < actions.size()) break;
+            System.out.println("Nieprawidłowy wybór!");
+        }
+    }
 
+    public static <T> T input(Class<T> type) {
+        Scanner in = new Scanner(System.in);
+        String opt = in.nextLine();
+
+        if (type.equals(Integer.class)) {
+            try {
+                return type.cast(Integer.parseInt(opt));
+            }   catch (Exception e) {
+                return type.cast(-1);
+            }
+        }
+
+        return type.cast(opt);
     }
 }
