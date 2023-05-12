93bbf3fb804515940959a2cec74f658be2146daa
jakubdziadkowiec17 <jakub.dziadkowiec17@gmail.com>
2023-05-12 16:44:49 +0200

    commit2

--- src/main/java/pl/edu/pk/student/carrots/cinema/actions/LoginAction.java
+++ src/main/java/pl/edu/pk/student/carrots/cinema/actions/LoginAction.java
@@ -1,13 +1,51 @@
 package pl.edu.pk.student.carrots.cinema.actions;
+import java.util.Scanner;
+public class LoginAction implements Action
+{
+    private class Login
+    {
+        private String login;
+        private String password;
 
-public class LoginAction implements Action {
+        Login(String login, String password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        private boolean ifLoginTrue(String login, String password)
+        {
+            if (this.login==login && this.password==password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
     @Override
     public String getTitle() {
         return "Zaloguj się";
     }
 
     @Override
-    public void doAction() {
-        // todo
+    public void doAction()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter login: ");
+        String login1 = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password1 = scanner.nextLine();
+        Login login = new Login(login1, password1);
+
+        if (login.ifLoginTrue(login1, password1)==true)
+        {
+            System.out.println("Login successful!");
+        } else
+        {
+            System.out.println("Invalid login details.");
+        }
     }
 }
